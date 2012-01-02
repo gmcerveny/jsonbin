3dd542f5429265584083c1f1bf99df5042c871d5
Greg Cerveny <greg.cerveny@gmail.com>
2012-01-02 16:25:14 -0600

    making it get/set

--- web.js
+++ web.js
@@ -1,10 +1,21 @@
 PORT = 8000;
 
 var http = require ('http');
+var url = require('url');
 
-var s = http.createServer(function(req, res){ 
-	res.writeHead(200, { 'content-type' : 'text/plain' });
-	res.end("Hello world\n");
+var message = '{"message":"Hello World"}';
+
+var s = http.createServer(function(request, response){ 
+	var urlParts = url.parse(request.url);
+	var urlAction = urlParts.pathname.substr(1,3);
+
+	if (urlAction == 'set') {
+		var urlValue = urlParts.pathname.substr(5);
+		message = unescape(urlValue);
+	}
+	
+	response.writeHead(200, { 'content-type' : 'text/plain' });
+	response.end(message);
 });
 
 s.listen(process.env.PORT || PORT);
